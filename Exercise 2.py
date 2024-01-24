ee984e591a22d28ed310063b7fff9ad41c70d517
Jazper26 <jazperhaapin89Gmail.com>
2024-01-25 00:34:57 +0800

    Finding how to apply sum with range

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,8 +1,24 @@
 
+pnum = 0
+
+for num in range(10):
+
+    for i in range(1, 10 + 1, 1):
+        sum = pnum + i
+
+    print ("Printing current and previous number sum in a range(10)")
+    print ("Current Number: " ,num)
+    print ("Sum: " ,sum)
 
 
-for num in range (10):
-    print (num)
+
+
+#for i in range (10):
+ #   for num2 in range (0, 10, ):
+
+
+
+# print ("Current Number: " ,num "Previous Number: " ,num2 "Sum: ", sum)
 
 
 
